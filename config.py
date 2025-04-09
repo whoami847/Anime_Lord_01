2cab68a545d41b6c0ad37041382e3e6a671173bb
whoami847 <mrbeastbangla069@gmail.com>
2025-04-09 17:33:27 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,10 @@ import os
 API_ID = int(os.getenv("API_ID", "28774737"))
 API_HASH = os.getenv("API_HASH", "851190ab85bb0b6dd547fff8e3c35b73")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7352104242:AAEpIiqsTduGBYON09wYdK-T9JLXBw7JdJE")
-PLUGIN_FOLDER = "handlers"
 
 ADMINS = [123456789]  # Your Telegram user ID
 CHANNELS = ["@log_chana", "@just_test_only_0"]
-
 AUTO_REPLY = {
     "text": "Hello! I am alive."
 }
-
 BANNED_USERS = set()
