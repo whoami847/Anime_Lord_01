c034abd8fc79d8f64eebdf760835384f62f33a1a
whoami847 <mrbeastbangla069@gmail.com>
2025-04-09 15:08:29 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "28774737"))
+API_HASH = os.getenv("API_HASH", "851190ab85bb0b6dd547fff8e3c35b73")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7352104242:AAEpIiqsTduGBYON09wYdK-T9JLXBw7JdJE")
 PLUGIN_FOLDER = "handlers"
 
 ADMINS = [123456789]  # Your Telegram user ID
-CHANNELS = ["@yourchannel1", "@yourchannel2"]
+CHANNELS = ["@log_chana", "@just_test_only_0"]
 
 AUTO_REPLY = {
     "text": "Hello! I am alive."
