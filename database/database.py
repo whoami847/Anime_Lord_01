f70d1d6662f05e6f7bb7c6d75ea1e74bd4d7ec3f
whoami847 <mrbeastbangla069@gmail.com>
2025-04-09 17:31:10 +0600

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,14 +1,25 @@
-import asyncio
+from pymongo import MongoClient
 
-# Dummy in-memory store
-users_db = set()
-bulk_files_db = {}
+class LogsDB:
+    def __init__(self, db_url):
+        self.client = MongoClient(db_url)
+        self.db = self.client["AnimeLordDB"]
+        self.collection = self.db["logs"]
 
-async def save_bulk_files(user_id, file_details):
-    if user_id not in bulk_files_db:
-        bulk_files_db[user_id] = []
-    bulk_files_db[user_id].append(file_details)
-    users_db.add(user_id)
+    def add_log(self, log_message):
+        self.collection.insert_one({"log": log_message})
 
-async def get_user_count():
-    return len(users_db)
+    def get_logs(self):
+        return list(self.collection.find())
+
+class UserDB:
+    def __init__(self, db_url):
+        self.client = MongoClient(db_url)
+        self.db = self.client["AnimeLordDB"]
+        self.collection = self.db["users"]
+
+    def add_user(self, user_id, username):
+        self.collection.insert_one({"user_id": user_id, "username": username})
+
+    def get_user(self, user_id):
+        return self.collection.find_one({"user_id": user_id})
